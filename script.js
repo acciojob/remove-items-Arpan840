bdaaab7c02301c01bb4c7974ec8755800a82669b
Arpan840 <123809535+Arpan840@users.noreply.github.com>
2023-07-20 23:41:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let button= document.querySelector('.button');
+let colorSelect= document.getElementById('colorSelect')
+function removeColor(){
+	let selectedIndex= colorSelect.selectedIndex;
+	if(selectedIndex!== -1)
+	{
+	colorSelect.remove(selectedIndex)}
+}
+button.addEventListener('click', removeColor)
